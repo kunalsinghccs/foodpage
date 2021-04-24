95edaffdfc322c79a9fcc0e3ef1342810b2c34bf
kunalsinghccs <79956365+kunalsinghccs@users.noreply.github.com>
2021-04-24 13:29:45 +0530

    initial upload v1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        autoplaySpeed:1000,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    });
+});
